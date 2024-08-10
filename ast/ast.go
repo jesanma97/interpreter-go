67c23877461d688033689f01e2f721895815f104
Jesús Ssngonzalo Martínez <jesanma97@gmail.com>
2024-08-10 18:10:06 +0200

    Parse return statement

--- ast/ast.go
+++ ast/ast.go
@@ -31,6 +31,15 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
